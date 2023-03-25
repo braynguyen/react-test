8299d58d8cafee057b9c5f2fd0c0088e0bb22dd4
Logan Warren <51924513+LoganWarren@users.noreply.github.com>
2023-03-24 21:47:23 -0400

    Index2 and Main

---
+++ src/main.js
@@ -0,0 +1,81 @@
+const startButton = document.getElementById("startButton");
+const stopButton = document.getElementById("stopButton");
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+let isCameraOn = false;
+
+async function setupCamera() {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    video: { width: 640, height: 480 },
+    audio: false,
+  });
+  video.srcObject = stream;
+  return new Promise((resolve) => {
+    video.onloadedmetadata = () => {
+      resolve(video);
+    };
+  });
+}
+
+async function detectHands() {
+  const model = await handpose.load();
+  video.play();
+
+  const keypointsDiv = document.getElementById("keypoints");
+
+  async function detect() {
+    const predictions = await model.estimateHands(video);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    keypointsDiv.innerHTML = ""; // Clear previous keypoints
+
+    for (let i = 0; i < predictions.length; i++) {
+      const keypoints = predictions[i].landmarks;
+
+      // Display keypoints below the camera
+      const keypointsText = `Hand ${i + 1}: ${JSON.stringify(keypoints)}`;
+      const keypointsElement = document.createElement("pre");
+      keypointsElement.textContent = keypointsText;
+      keypointsDiv.appendChild(keypointsElement);
+
+      // Draw dots at each keypoint
+      for (let j = 0; j < keypoints.length; j++) {
+        ctx.beginPath();
+        ctx.arc(keypoints[j][0], keypoints[j][1], 5, 0, 2 * Math.PI);
+        ctx.fillStyle = "red";
+        ctx.fill();
+      }
+    }
+
+    if (isCameraOn) {
+      // Call the detect function again after a 2-second buffer
+      setTimeout(() => {
+        requestAnimationFrame(detect);
+      }, 2000);
+    }
+  }
+
+  detect();
+}
+
+async function main() {
+  await setupCamera();
+
+  startButton.addEventListener("click", () => {
+    isCameraOn = true;
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    video.style.display = "block";
+    detectHands();
+  });
+
+  stopButton.addEventListener("click", () => {
+    isCameraOn = false;
+    startButton.disabled = false;
+    stopButton.disabled = true;
+video.style.display = "none";
+});
+}
+
+main();
